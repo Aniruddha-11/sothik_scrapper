2bdd04b4f3c05cc09f12e2ba9947a4f19380899c
Aniruddha-11 <aniruddha.mukherjee@ebiw.com>
2025-03-11 14:41:53 +0530

    Deploy3

--- app.py
+++ app.py
@@ -10,5 +10,5 @@ app.register_blueprint(file_api)
 
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))  # Use PORT from environment or default to 5000
+    port = int(os.getenv("PORT", 5000))  # Default to 5000 if PORT is not set
     app.run(host="0.0.0.0", port=port)
